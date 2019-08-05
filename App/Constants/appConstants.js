4f06cb3cbbf62625b7585009e7c1980598187fd0
vishal <vishalrocks73139@gmail.com>
2019-08-05 13:37:44 +0530

    Update appConstants.js

--- App/Constants/appConstants.js
+++ App/Constants/appConstants.js
@@ -2,7 +2,7 @@ export const monthNames = ["January", "February", "March", "April", "May", "June
   "July", "August", "September", "October", "November", "December"
 ];
 
-export const expenseKeyWords = ["debited", "paid", "thank you for using", "thanks for using", "spent", "accepted your request", "accepted request", "request accepted", "payment of", "You've spent"];
+export const expenseKeyWords = ["debited", "paid", "thank you for using", "thanks for using", "spent", "accepted your request", "accepted request", "request accepted", "payment of", "You've spent","thanks for paying"];
 
 export const ignoreExpenseKeyWords = ["ignore if paid", "ignore if already paid", "will be debited", "requested", "failed", "fail", "claim", "gift"]
 
